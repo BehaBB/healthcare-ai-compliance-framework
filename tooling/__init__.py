3ff035ceebeabdfb933d72b0b771aab7f6ead80e
BehaB <91378877+BehaBB@users.noreply.github.com>
2026-04-06 04:17:49 +0800

    Update __init__.py

--- tooling/__init__.py
+++ tooling/__init__.py
@@ -1,1 +1,7 @@
+# tooling/__init__.py
+# Делаем tooling Python-пакетом
+from .policy_loader import YAMLPolicyEngine
+from .policy_engine import PolicyEngine
+from .audit import AuditLogger
 
+__all__ = ['YAMLPolicyEngine', 'PolicyEngine', 'AuditLogger']
